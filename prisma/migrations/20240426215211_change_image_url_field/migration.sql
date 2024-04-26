5848662539108220e175f4a0bba729835da9d6c5
Rafael de Lima <rafalimabcl@gmail.com>
2024-04-26 18:54:10 -0300

    fix: fixed imageUrl name

---
+++ prisma/migrations/20240426215211_change_image_url_field/migration.sql
@@ -0,0 +1,22 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `imageURL` on the `Category` table. All the data in the column will be lost.
+  - You are about to drop the column `imageURL` on the `Product` table. All the data in the column will be lost.
+  - You are about to drop the column `imageURL` on the `Restaurant` table. All the data in the column will be lost.
+  - Added the required column `imageUrl` to the `Category` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `imageUrl` to the `Product` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `imageUrl` to the `Restaurant` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Category" DROP COLUMN "imageURL",
+ADD COLUMN     "imageUrl" TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Product" DROP COLUMN "imageURL",
+ADD COLUMN     "imageUrl" TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Restaurant" DROP COLUMN "imageURL",
+ADD COLUMN     "imageUrl" TEXT NOT NULL;
