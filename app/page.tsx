8d12bf7f23a783287864192d1334c5c00d3d5694
Rafael de Lima <rafalimabcl@gmail.com>
2024-06-19 22:53:06 -0300

    feat: add initial search component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
 import Header from "./_components/header";
+import Search from "./_components/search";
 
   const Home = () => {
     return ( 
-      <Header/>
-     );
+      <>
+        <Header/>
+        <div className="px-5 pt-6">
+          <Search/>
+        </div>
+      </>
+      
+      
+    );
   }
    
   export default Home;
