707c5b6d92862ebac7ea999a3e83236771197e17
Rafael de Lima <rafalimabcl@gmail.com>
2024-06-21 22:59:40 -0300

    feat: add promo banner to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+
+import Image from "next/image";
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search";
@@ -12,6 +14,18 @@ import Search from "./_components/search";
 
         <div className="px-5 pt-6">
           <CategoryList />
+        </div>
+
+        <div className="px-5 pt-6">
+          <Image 
+            src="/promo-banner-01.png" 
+            alt="Até 30% de descontos em pizza"
+            height={0}
+            width={0}
+            className="w-full h-auto object-contain"
+            sizes="100vw"
+            quality={100}
+          />
         </div>
       </>
       
